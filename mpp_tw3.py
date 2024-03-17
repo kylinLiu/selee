dc46b4b0f237fc049c98d68d478ad3d808c66859
Liumumu <247651305@qq.com>
2024-03-17 10:05:31 +0800

    Add files via upload

---
+++ mpp_tw3.py
@@ -0,0 +1,475 @@
+order_list = [
+
+    # {
+    #     "name": "李昶諺",
+    #     "phone": "+886968902286",
+    #     "email": "1228597880@qq.com",
+    #     "rule": "110010000",
+    # },
+    #
+    # {
+    #     "name": "黃雨歆",
+    #     "phone": "+886900204256",
+    #     "email": "hyx20070912@gmail.com",
+    #     "rule": "1100011000100",
+    # },
+    # {
+    #     "xy": "别发中通",
+    #     "name": "林禹彤",
+    #     "phone": "+886953878677",
+    #     "email": "e9090702@gmail.com",
+    #     "rule": "0100001010100",
+    # },
+    {
+        "xy": "浮生醉风清",
+        "name": "李雨欣",
+        "phone": "+886937673012",
+        "email": "",
+        "rule": "0100000000000",
+    },
+    # {
+    #     "xy": "水果味的桃子",
+    #     "name": "鄭乃榕",
+    #     "phone": "+886966376531",
+    #     "email": "",
+    #     "rule": "0010000000000",
+    # },
+    {
+        "xy": "别发中通",
+        "name": "鍾瑞展",
+        "phone": "+886986862729",
+        "email": "1228597880@qq.com",
+        "rule": "0100111010100",
+    },
+    {
+        "name": "黃薏璇",
+        "phone": "+886986633185",
+        "email": "",
+        "rule": "1100010000100",
+    },
+    # {
+    #     "name": "顏思嫻",
+    #     "phone": "+886916932737",
+    #     "email": "",
+    #     "rule": "1100000000100",
+    # },
+    # {
+    #     "name": "任馨怡",
+    #     "phone": "+886918007409",
+    #     "email": "",
+    #     "rule": "1000000000100",
+    # },
+    # {
+    #     "name": "康珮妮",
+    #     "phone": "+886916932737",
+    #     "email": "",
+    #     "rule": "1001011100100",
+    # },
+    # {
+    #     "name": "何妤涵",
+    #     "phone": "+886988322140",
+    #     "email": "",
+    #     "rule": "100000000",
+    # },
+    # {
+    #     "name": "李雨欣",
+    #     "phone": "+886937673012",
+    #     "email": "",
+    #     "rule": "1100000000100",
+    # },
+    # {
+    #     "name": "彭婉蓉",
+    #     "phone": "+886909222917",
+    #     "email": "s013765@yahoo.com.tw",
+    #     "rule": "1100000000100",
+    # },
+    {
+        "name": "范雅淳",
+        "phone": "+886965723224",
+        "email": "",
+        "rule": "1100000000100",
+    },
+    {
+        "name": "张小姐",
+        "phone": "+886976121610",
+        "email": "",
+        "rule": "1110010000100",
+    },
+    # {
+    #     "name": "鄭宇軒",
+    #     "phone": "+886958308680",
+    #     "email": "",
+    #     "rule": "1000000000100",
+    # },
+    # {
+    #     "name": "張靜雯",
+    #     "phone": "+886958114044",
+    #     "email": "",
+    #     "rule": "010000001",
+    # },
+    # {
+    #     "name": "潘佳琪",
+    #     "phone": "+886981366882",
+    #     "email": "",
+    #     "rule": "000000000000",
+    # },
+    # {
+    #     "xy": "帅哥帅哥",
+    #     "name": "黃珮瑤",
+    #     "phone": "+886900675279",
+    #     "email": "peiyao530@gmail.com",
+    #     "rule": "1000000000000",
+    # },
+    # {
+    #     "xy": "Fellows",
+    #     "name": "黃薏璇",
+    #     "phone": "+886953857989",
+    #     "email": "",
+    #     "rule": "010000001010",
+    # },
+    # {
+    #     "name": "靳可馨",
+    #     "phone": "+8617719903602",
+    #     "email": "",
+    #     "rule": "0110010000100",
+    # },
+    # {
+    #     "name": "董成庭",
+    #     "phone": "+8617719903602",
+    #     "email": "",
+    #     "rule": "0110010000100",
+    # },
+    # {
+    #     "xy": "我一张卡也不会再买",
+    #     "name": "陳麗銀",
+    #     "phone": "+886971880985",
+    #     "email": "",
+    #     "rule": "0000000000000",
+    # },
+    # B
+    {
+        "xy": "不吃香芋派",
+        "name": "龍芊",
+        "phone": "+8615515072231",
+        "email": "",
+        "rule": "0000000001010",
+    },
+    # {
+    #     "xy": "一口西瓜",
+    #     "name": "歐陽萱",
+    #     "phone": "+886976246740",
+    #     "email": "",
+    #     "rule": "0000000001000",
+    # },
+    {
+        "xy": "橙子焦糖",
+        "name": "袁亮天",
+        "phone": "+8613203616383",
+        "email": "",
+        "rule": "1110000001000",
+    },
+    # {
+    #     "xy": "水果味的桃子",
+    #     "name": "陳泓瑄",
+    #     "phone": "+886954016316",
+    #     "email": "",
+    #     "rule": "0010000000000",
+    # },
+    # {
+    #     "xy": "Ambition",
+    #     "name": "陳泓瑄",
+    #     "phone": "+886954016316",
+    #     "email": "",
+    #     "rule": "1000000001000",
+    # },
+    {
+        "xy": "妖妖不吃药",
+        "name": "黃梓晴",
+        "phone": "+886971772781",
+        "email": "",
+        "rule": "0110010010000",
+    },
+    {
+        "xy": "薄荷糖管家peps",
+        "name": "湯蘊庭",
+        "phone": "+886988895143",
+        "email": "",
+        "rule": "0110010000000",
+    },
+    {
+        "xy": "Zxyw",
+        "name": "袁亮天",
+        "phone": "+886902203521",
+        "email": "",
+        "rule": "1110010000100",
+    },
+    {
+        "xy": "人傻钱多的笨蛋小米粥",
+        "name": "熊妮",
+        "phone": "+886966902167",
+        "email": "",
+        "rule": "1100010001001",
+    },
+    {
+        "xy": "一逸逸逸",
+        "name": "陳宗霆",
+        "phone": "+886988239915",
+        "email": "",
+        "rule": "1100000001000",
+    },
+    {
+        "xy": "南瓜玉米",
+        "name": "鄭宇軒",
+        "phone": "+886958308680",
+        "email": "",
+        "rule": "1000000001000",
+    },
+    {
+        "xy": "小泉桃子",
+        "name": "李雨欣",
+        "phone": "+886937673012",
+        "email": "",
+        "rule": "1000000000000",
+    },
+    {
+        "xy": "努力的小天才",
+        "name": "徐子雁",
+        "phone": "+886960587629",
+        "email": "",
+        "rule": "1110000001000",
+    },
+    {
+        "xy": "圆佑百货店喜欢您来",
+        "name": "王瑞蔻",
+        "phone": "+886937087462",
+        "email": "",
+        "rule": "0100000001000",
+    },
+    {
+        "xy": "事妈白菜精远离我",
+        "name": "張靜雯",
+        "phone": "+886958114044",
+        "email": "",
+        "rule": "0100000010000",
+    },
+    {
+        "xy": "已经写明的内容不会回复",
+        "name": "李昶諺",
+        "phone": "+886968902286",
+        "email": "e9090702@gmail.com",
+        "rule": "0100100000000",
+    },
+    {
+        "xy": "不吃了我真不吃了",
+        "name": "余念恩",
+        "phone": "+886935960504",
+        "email": "",
+        "rule": "1000000001000",
+    },
+    {
+        "xy": "希尔星蹦极田螺",
+        "name": "江東",
+        "phone": "+886917261046",
+        "email": "",
+        "rule": "1110110101000",
+    },
+    {
+        "xy": "我一张小卡都不会再买",
+        "name": "張筱筑",
+        "phone": "+886989193959",
+        "email": "",
+        "rule": "0000000000000",
+    },
+    {
+        "xy": "帅哥帅哥",
+        "name": "黃證維",
+        "phone": "+886921344833",
+        "email": "",
+        "rule": "0000000000000",
+    },
+    {
+        "xy": "别收藏了心动不如行动大家",
+        "name": "張筱筑",
+        "phone": "+886989193959",
+        "email": "",
+        "rule": "0000000000000",
+    },
+    {
+        "xy": "qnsya",
+        "name": "伍羽",
+        "phone": "+886968399708",
+        "email": "",
+        "rule": "1100010001000",
+    },
+    {
+        "xy": "lucky_锅仔",
+        "name": "黄立爱",
+        "phone": "+886938805598",
+        "email": "2125863695@qq.com",
+        "rule": "1100000001000",
+    },
+    {
+        "xy": "peachM",
+        "name": "张小姐",
+        "phone": "+886976121610",
+        "email": "",
+        "rule": "1110010001000",
+    },
+]
+"""
+peachM
+不要当天:1
+不要15点:1
+不要11点:1
+必须周末:0
+必须工作日:0
+工作日17点前不要:1
+工作日19点前不要:0
+
+0976121610张小姐
+"""
+rule_idx ={'2024-03-17_11:00': [0, 20, 21, 22], '2024-03-17_13:00': [0, 8, 20, 21, 22], '2024-03-17_15:00': [20, 21, 22], '2024-03-17_17:00': [0, 7, 8, 16, 20, 21, 22], '2024-03-17_19:00': [0, 7, 8, 16, 20, 21, 22], '2024-03-18_11:00': [0, 3, 13, 16, 17, 20, 21, 22], '2024-03-18_13:00': [0, 3, 13, 16, 17, 20, 21, 22], '2024-03-18_15:00': [13, 20, 21, 22], '2024-03-18_17:00': [0, 2, 3, 4, 7, 8, 9, 13, 16, 17, 20, 21, 22], '2024-03-18_19:00': [0, 1, 2, 3, 4, 7, 8, 9, 13, 16, 17, 20, 21, 22], '2024-03-19_11:00': [0, 11, 12, 13, 15, 16, 17, 18, 20, 21, 22, 24], '2024-03-19_13:00': [0, 6, 11, 12, 13, 14, 15, 16, 17, 18, 20, 21, 22, 24], '2024-03-19_15:00': [12, 13, 18, 20, 21, 22], '2024-03-19_17:00': [0, 6, 7, 8, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25], '2024-03-19_19:00': [0, 6, 7, 8, 10, 11, 12, 13, 14, 15, 16, 17, 18, 20, 21, 22, 23, 24, 25], '2024-03-20_11:00': [0, 5, 11, 12, 13, 15, 16, 17, 18, 20, 21, 22, 24], '2024-03-20_13:00': [0, 5, 6, 11, 12, 13, 14, 15, 16, 17, 18, 20, 21, 22, 24], '2024-03-20_15:00': [5, 12, 13, 18, 20, 21, 22], '2024-03-20_17:00': [0, 5, 6, 7, 8, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25], '2024-03-20_19:00': [0, 5, 6, 7, 8, 10, 11, 12, 13, 14, 15, 16, 17, 18, 20, 21, 22, 23, 24, 25], '2024-03-21_11:00': [0, 11, 12, 13, 15, 16, 17, 18, 20, 21, 22, 24], '2024-03-21_13:00': [0, 6, 11, 12, 13, 14, 15, 16, 17, 18, 20, 21, 22, 24], '2024-03-21_15:00': [12, 13, 18, 20, 21, 22], '2024-03-21_17:00': [0, 6, 7, 8, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25], '2024-03-21_19:00': [0, 6, 7, 8, 10, 11, 12, 13, 14, 15, 16, 17, 18, 20, 21, 22, 23, 24, 25], '2024-03-22_11:00': [0, 5, 11, 12, 13, 15, 16, 17, 18, 20, 21, 22, 24], '2024-03-22_13:00': [0, 5, 6, 11, 12, 13, 14, 15, 16, 17, 18, 20, 21, 22, 24], '2024-03-22_15:00': [5, 12, 13, 18, 20, 21, 22], '2024-03-22_17:00': [0, 5, 6, 7, 8, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25], '2024-03-22_19:00': [0, 5, 6, 7, 8, 10, 11, 12, 13, 14, 15, 16, 17, 18, 20, 21, 22, 23, 24, 25], '2024-03-23_11:00': [0, 5, 10, 11, 12, 13, 15, 16, 18, 20, 21, 22, 23, 24], '2024-03-23_13:00': [0, 5, 6, 7, 8, 10, 11, 12, 13, 14, 15, 16, 18, 20, 21, 22, 23, 24, 25], '2024-03-23_15:00': [5, 12, 13, 18, 20, 21, 22], '2024-03-23_17:00': [0, 5, 6, 7, 8, 10, 11, 12, 13, 14, 15, 16, 18, 20, 21, 22, 23, 24, 25], '2024-03-23_19:00': [0, 5, 6, 7, 8, 10, 11, 12, 13, 14, 15, 16, 18, 20, 21, 22, 23, 24, 25], '2024-03-24_11:00': [0, 5, 10, 11, 12, 13, 15, 16, 18, 20, 21, 22, 23, 24], '2024-03-24_13:00': [0, 5, 6, 7, 8, 10, 11, 12, 13, 14, 15, 16, 18, 20, 21, 22, 23, 24, 25], '2024-03-24_15:00': [5, 12, 13, 18, 20, 21, 22], '2024-03-24_17:00': [0, 5, 6, 7, 8, 10, 11, 12, 13, 14, 15, 16, 18, 20, 21, 22, 23, 24, 25], '2024-03-24_19:00': [0, 5, 6, 7, 8, 10, 11, 12, 13, 14, 15, 16, 18, 20, 21, 22, 23, 24, 25]}
+
+import mitmproxy.http
+from mitmproxy import ctx, http
+from urllib.parse import urlparse
+import json
+
+import re
+import requests
+# mitmdump.exe -s E:\tmp\fastapi_demo\mpp_tw2.py
+class Counter:
+    def __init__(self):
+        self.num = 0
+
+    def response(self, flow: mitmproxy.http.HTTPFlow):
+
+        if ('bookingV5' in flow.request.url
+                and 'OPR/' not in flow.request.headers['user-agent']
+        ):
+            ctx.log.info("vvvvvvvvvvvvvvvvvvvvvvvvvvv")
+            content = flow.response.get_text()
+            bbd = '{"default":{"2024-03-23":{"times":{"19:00":[1,2]},"dinerTime":105,"maxBookingSize":2,"minBookingSize":1,"status":"open"}},"-N3JUXHY_OqTD5B89GUP":{"2024-03-23":{"times":{"19:00":[1,2]},"dinerTime":105,"maxBookingSize":2,"minBookingSize":1,"status":"open"}}}'
+            content = re.sub(r'getBookingCapacities\(queries.*?body', f"getBookingCapacities(queries){{return {bbd}",
+                             content)
+            flow.response.set_text(content)
+
+    # def response(self, flow: mitmproxy.http.HTTPFlow):
+    #
+    #     if 'booking-capacitiesV3' in flow.request.url:
+    #         ctx.log.error(flow.request.url)
+    #         content = flow.response.get_text()
+    #         content = content.replace('full', 'open')
+    #         content = content.replace('[]', '[1,2]')
+    #         flow.response.set_text(content)
+
+    # elif ('booking/-MeNcbDasiIykiow2Hfb:inline-live-2/-N3JQxh1vIZe9tECk0Pg' in flow.request.url
+    #       and flow.response.status_code == 200
+    # ):
+    #
+    #     content = flow.response.get_text()
+    #     content = content.replace("</html>",
+    #                               # "<script>function executeTask(){document.getElementsByClassName('sc-hHLeRK jmEJIc')[0].click()};var now=new Date();var timeToNextMidnight=(new Date(now.getFullYear(),now.getMonth(),now.getDate(),8,40)).getTime()-now.getTime();console.log(timeToNextMidnight);if(timeToNextMidnight<0){executeTask();}else{setTimeout(executeTask,timeToNextMidnight);}</script></html>"
+    #                               "<script>function executeTask(){document.getElementsByClassName('sc-hHLeRK jmEJIc')[0].click()};var now=new Date();var timeToNextMidnight=(new Date(now.getFullYear(),now.getMonth(),now.getDate()+1)).getTime()-now.getTime();console.log('00');if(timeToNextMidnight<0){executeTask();}else{setTimeout(executeTask,timeToNextMidnight);}</script></html>"
+    #                               )
+    #     flow.response.set_text(content)
+    # # ctx.log.info(f"{flow.response.get_text()}")
+
+    def request(self, flow: mitmproxy.http.HTTPFlow):
+        # self.num = self.num + 1
+
+        if 'api/reservations/booking' in flow.request.url:
+            with open(r'D:\tmp\k_kk') as f:
+                content = f.read()
+            content = content.replace("\n", "")
+            date, time, idx = content.split("_")
+            user = order_list[int(idx)]
+            # if date in gzr and time == '19:00':
+            #     print("xs")
+            content = flow.request.get_text()
+            # ctx.log.info(content)
+            # content = content.replace("2024-03-06", "2024-03-05")
+            content = re.sub(r'"date":"\d{4}-\d{2}-\d{2}"', f'"date":"{date}"', content)
+            content = re.sub(r'"time":"\d{2}:\d{2}"', f'"time":"{time}"', content)
+            content = re.sub(r'"name":".*?"', f'"name":"{user["name"]}"', content)
+            content = re.sub(r'"phone":".*?"', f'"phone":"{user["phone"]}"', content)
+            content = re.sub(r'"email":".*?"', f'"email":"{user["email"]}"', content)
+            # ctx.log.info(content)
+            flow.request.set_text(content)
+        elif ('booking-capacitiesV3' in flow.request.url
+                and '1008611' not in flow.request.url):
+            # flow.kill()
+            # return
+            # content = flow.request.get_text()
+            # # ctx.log.info(content)
+            # # content = content.replace("2024-03-06", "2024-03-05")
+            # content = re.sub(r'"date":"\d{4}-\d{2}-\d{2}"', '"date":"2024-03-11"', content)
+            ctx.log.info("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+            # flow.request.set_text(content)
+
+            if 'OPR/' in flow.request.headers['user-agent']:
+                ctx.log.info("bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb")
+                headers = dict(flow.request.headers)
+                cookies = "; ".join(f"{k}={v}" for k, v in dict(flow.request.cookies).items())
+                # content = dict()
+                # recaptchaToken = flow.request.json()['recaptchaToken']
+                recaptchaToken = ''
+                # ctx.log.info(f"{urlparse(flow.request.url).query}")
+                # ctx.log.info(f"{dict(flow.request.headers)}")
+                # doall(headers)
+                headers["cookie"] = cookies
+                # flow.kill()
+                url = "http://127.0.0.1/inlineapp/api/orderadd"
+                # ctx.log.info(headers)
+
+                form_data = {
+                    "header": json.dumps(headers),
+                    "recaptchaToken": recaptchaToken,
+                }
+                response = requests.post(url, data=form_data)
+                ctx.log.info("ccccccccccccccccccccccccccccccccccc")
+                ctx.log.info(response.text)
+
+            # flow.response = http.Response.make(
+            #     200,
+            #     '{"default":{"2024-03-11":{"times":{"19:00":[1,2]},"dinerTime":105,"maxBookingSize":2,"minBookingSize":1,"status":"open"}},"-N3JUXHY_OqTD5B89GUP":{"2024-03-11":{"times":{"19:00":[1,2]},"dinerTime":105,"maxBookingSize":2,"minBookingSize":1,"status":"open"}}}',
+            #     {'content-type': "application/json; charset=utf-8"}
+            # )
+        # flow.kill()
+    # def request(self, flow: mitmproxy.http.HTTPFlow):
+    #     # self.num = self.num + 1
+    #     flow.request.get_text()
+    #     if 'api/reservations/booking' in flow.request.url:
+    #         # flow.kill()
+    #         content = flow.request.get_text()
+    #         # ctx.log.info(content)
+    #         # content = content.replace("2024-03-06", "2024-03-05")
+    #         content = re.sub(r'"date":"\d{4}-\d{2}-\d{2}"', '"date":"2024-03-07"', content)
+    #         # ctx.log.info(content)
+    #         flow.request.set_text(content)
+
+    # def request(self, flow: mitmproxy.http.HTTPFlow):
+    #     # self.num = self.num + 1
+    #     if 'api/reservations/booking' in flow.request.url:
+    #         ctx.log.error("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
+    #         ctx.log.error("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
+    #         ctx.log.error("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
+    #         ctx.log.error("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
+    #         ctx.log.error("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
+    #         ctx.log.error(dict(flow.request.cookies))
+    #         # path = flow.request.url
+    #
+    #         cookies = "; ".join(f"{k}={v}" for k, v in dict(flow.request.cookies).items())
+    #         ctx.log.info(cookies)
+    #         headers = dict(flow.request.headers)
+    #         # ctx.log.info(f"{urlparse(flow.request.url).query}")
+    #         # ctx.log.info(f"{dict(flow.request.headers)}")
+    #         # doall(headers)
+    #         headers["cookie"] = cookies
+    #         with open(r'D:\tmp\tw_headers', 'w') as f:
+    #             f.write(json.dumps(headers))
+
+
+addons = [
+    Counter()
+]
